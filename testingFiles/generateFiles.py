de82263c2b6a5847bcfbcbc7d427f4502b488428
puneeth.kpati <puneeth.kpati@gmail.com>
2020-11-25 16:54:14 +1100

    testing files

--- testingFiles/generateFiles.py
+++ testingFiles/generateFiles.py
@@ -4,4 +4,5 @@ sizes = ['100K', '500K', '1M', '5M', '10M']
 
 for size in sizes:
 	for count in range(30):
-		os.system(f'truncate -s {size} {size}_{count}.txt')
+		os.system(f'truncate -s {size} {size}B_{count}.txt')
+		print(f'{size}B_{count}.txt')
