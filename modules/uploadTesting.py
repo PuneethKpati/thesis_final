246195a74883f12322179b1017157824ba8da9ee
puneeth.kpati <puneeth.kpati@gmail.com>
2020-11-25 19:12:38 +1100

    sorted uploading

--- modules/uploadTesting.py
+++ modules/uploadTesting.py
@@ -7,31 +7,31 @@ import xlsxwriter
 fileStorage = FileStorage()
 testingFiles = '../testingFiles/'
 fileNames = sorted(os.listdir(testingFiles))
-print(fileNames)
 
-# workbook = xlsxwriter.Workbook('./data/upload.xlsx')
-# worksheet = workbook.add_worksheet()
-# worksheet.write('A1', 'fileName')
-# worksheet.write('B1', 'Size')
-# worksheet.write('C1', 'time')
-# worksheet.write('D1', 'start')
-# worksheet.set_column('A:C', 25)
 
-# row = 2
-# for file in fileNames:
-# 	filePath = testingFiles+file
+workbook = xlsxwriter.Workbook('./data/upload.xlsx')
+worksheet = workbook.add_worksheet()
+worksheet.write('A1', 'fileName')
+worksheet.write('B1', 'Size')
+worksheet.write('C1', 'time')
+worksheet.write('D1', 'start')
+worksheet.set_column('A:C', 25)
 
-# 	start = datetime.now()
-# 	info = fileStorage.upload(filePath)
-# 	diff = datetime.now() - start
-# 	diffTime = diff.seconds+diff.microseconds/1000000
-# 	print(diffTime)
+row = 2
+for file in fileNames:
+	filePath = testingFiles+file
 
-# 	worksheet.write(row, 0, file)
-# 	worksheet.write(row, 1, info['Size'])
-# 	worksheet.write(row, 2, diffTime)
-# 	worksheet.write(row, 3, start.strftime("%d/%m/%Y - %H:%M:%S"))
+	start = datetime.now()
+	info = fileStorage.upload(filePath)
+	diff = datetime.now() - start
+	diffTime = diff.seconds+diff.microseconds/1000000
+	print(diffTime)
 
-# 	row += 1
+	worksheet.write(row, 0, file)
+	worksheet.write(row, 1, info['Size'])
+	worksheet.write(row, 2, diffTime)
+	worksheet.write(row, 3, start.strftime("%d/%m/%Y - %H:%M:%S"))
 
-# workbook.close()
+	row += 1
+
+workbook.close()
